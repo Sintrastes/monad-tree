488b741bad51547c58eb6cb85699d756d865cbeb
Nathan Bedell <nbedell@tulane.edu>
2021-11-09 08:59:57 -0500

    Add mtl and transformer instances.

--- src/Control/Monad/Trans/Tree.hs
+++ src/Control/Monad/Trans/Tree.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE UndecidableInstances #-}
+
 {-|
 Module      : Control.Monad.Tree
 Description : Implementation of a non-deterministic tree monad.
@@ -14,14 +16,20 @@ Note that this implementation is still experimental, in the sense that the monad
 module Control.Monad.Trans.Tree where
 
 import Data.Functor
+import Control.Monad.IO.Class
+import Control.Monad.Trans.Class
+import Control.Monad.Error.Class
+import Control.Monad.Reader.Class
+import Control.Monad.State.Class
+import Control.Monad.Writer.Class
 
-data TreeM m n f a = 
+data TreeM n f m a = 
     Leaf a 
-  | Node n (m (f (TreeM m n f a)))
+  | Node n (m (f (TreeM n f m a)))
 
-newtype TreeT m n f a = TreeT { runTreeT :: m (TreeM m n f a) }
+newtype TreeT n f m a = TreeT { runTreeT :: m (TreeM n f m a) }
 
-bind :: (Monad m, Functor f, Traversable f) => TreeT m n f a -> (a -> TreeT m n f b) -> TreeT m n f b 
+bind :: (Monad m, Functor f, Traversable f) => TreeT n f m a -> (a -> TreeT n f m b) -> TreeT n f m b 
 bind x f = TreeT $ do
     x' <- runTreeT x
     case x' of
@@ -32,23 +40,49 @@ bind x f = TreeT $ do
                  (fmap return as))
         Leaf x -> runTreeT $ f x
 
-instance (Functor f, Functor m) => Functor (TreeM m n f) where
+instance (Functor f, Functor m) => Functor (TreeM n f m) where
     fmap f (Leaf x) = Leaf $ f x
     fmap f (Node l xs) = Node l (fmap (f <$>) <$> xs)
 
-instance (Functor f, Functor m) => Functor (TreeT m n f) where
+instance (Functor f, Functor m) => Functor (TreeT n f m) where
     fmap f x = TreeT $ runTreeT x <&> (\x' ->
             case x' of
                 Leaf v -> Leaf $ f v
                 Node l vs -> Node l (((f <$>) <$>) <$> vs)
         ) 
 
-instance (Monad m, Traversable f) => Applicative (TreeT m n f) where
+instance (Monad m, Traversable f) => Applicative (TreeT n f m) where
     pure x = TreeT $ pure $ Leaf x
     (<*>) fs xs = do
         f <- fs
         x <- xs
         pure (f x)
 
-instance (Traversable f, Monad m) => Monad (TreeT m n f) where
+instance (Traversable f, Monad m) => Monad (TreeT n f m) where
     (>>=) x f = bind x f
+
+instance MonadTrans (TreeT n f) where
+    lift x = TreeT $ Leaf <$> x
+
+instance (MonadIO m, Traversable f) => MonadIO (TreeT n f m) where
+    liftIO x = TreeT $ liftIO $ Leaf <$> x
+
+instance (MonadState s m, Traversable f) => MonadState s (TreeT n f m) where
+    get = lift get
+    put = lift . put
+
+-- TODO: Not sure how to define these instances.
+{-
+instance (MonadReader r m, Traversable f) => MonadReader r (TreeT n f m) where
+    ask = lift ask
+    local = undefined
+
+instance (MonadWriter w m, Traversable f) => MonadWriter w (TreeT n f m) where
+    tell   = lift . tell
+    listen = undefined
+    pass = undefined
+
+instance (MonadError e m, Traversable f) => MonadError e (TreeT n f m) where
+    throwError = lift throwError
+    catchError = undefined
+-}
